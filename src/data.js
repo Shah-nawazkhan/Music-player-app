0fe7500dcb01ea6508e804d15dd879959f3a04f0
Shahnawaz <shahnawazkhan333@live.com>
2024-02-09 21:19:27 +0500

    Added my project

---
+++ src/data.js
@@ -0,0 +1,74 @@
+
+import { v4 as uuidv4 } from "uuid"; 
+function chillHop() { 
+return [ 
+	{ 
+	name: "Sunrise Serenade", 
+	cover: 
+"https://cdn.pixabay.com/photo/2023/03/26/11/40/woman-7878192_1280.jpg", 
+	artist: " Harmony Harp", 
+	audio: 
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3", 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: true, 
+	}, 
+	{ 
+	name: "Urban Groove", 
+	cover: 
+		"https://cdn.pixabay.com/photo/2023/10/24/21/15/nature-8339115_1280.jpg", 
+	artist: "Beatmaster B", 
+	audio: 
+		"https://media.geeksforgeeks.org/wp-content/uploads/20231004184006/SoundHelix-Song-10.mp3", 
+	color: ["#EF8EA9", "#ab417f"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+	{ 
+	name: "Mystic Echo", 
+	cover: 
+"https://cdn.pixabay.com/photo/2023/09/01/23/33/motorcycles-8227939_1280.jpg", 
+	artist: " Harmony Harp", 
+	audio: 
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3", 
+	color: ["#CD607D", "#c94043"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+	{ 
+	name: "Electro Vibes", 
+	cover: 
+"https://cdn.pixabay.com/photo/2023/11/04/15/01/maple-8365123_1280.jpg", 
+	artist: "Synthwave Sensation", 
+	audio: 
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004191840/Zinda-Banda---Jawan-(1).mp3", 
+	color: ["#EF8EA9", "#ab417f"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+	{ 
+	name: "Jazzy Whispers", 
+	cover: 
+"https://cdn.pixabay.com/photo/2024/01/31/19/25/sunset-8544672_1280.jpg", 
+	artist: "Smooth Sax Serenade", 
+	audio: 
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004184006/SoundHelix-Song-10.mp3", 
+	color: ["#CD607D", "#c94043"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+	{ 
+	name: "Tropical Breez", 
+	cover: 
+"https://cdn.pixabay.com/photo/2023/10/26/11/17/flower-8342448_1280.jpg", 
+	artist: "Island Rhythms", 
+	audio: 
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004191840/Zinda-Banda---Jawan-(1).mp3", 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+]; 
+} 
+
+export default chillHop; 
